58f351fd98fdc8e7e728f89be39a6a07c8eba1d9
Josh Nowak <Joshnwk@yahoo.com>
2015-08-03 15:56:40 -0600

    add pred plots from Mark

---
+++ Plotting_Functions/prediciton_plots.R
@@ -0,0 +1,9 @@
+		#  Prediction Plots workflow for Idaho survival models
+		#  Josh Nowak
+		#  07/2015
+#################################################################################
+		#  Packages
+		require(ggplot2)
+		require(dplyr)
+#################################################################################
+		#  
